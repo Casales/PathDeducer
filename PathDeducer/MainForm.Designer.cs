779d84de1581f4806d87e6a7e3060fb4a2ccc3f7
Manuel Martin <martiman89@gmail.com>
2022-06-12 22:21:25 +0200

    Add project files.

---
+++ PathDeducer/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace PathDeducer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_SelectExcel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_SelectExcel
+            // 
+            this.button_SelectExcel.Location = new System.Drawing.Point(12, 12);
+            this.button_SelectExcel.Name = "button_SelectExcel";
+            this.button_SelectExcel.Size = new System.Drawing.Size(75, 23);
+            this.button_SelectExcel.TabIndex = 0;
+            this.button_SelectExcel.Text = "Select";
+            this.button_SelectExcel.UseVisualStyleBackColor = true;
+            this.button_SelectExcel.Click += new System.EventHandler(this.button_SelectExcel_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_SelectExcel);
+            this.Name = "MainForm";
+            this.Text = "Path Deducer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_SelectExcel;
+    }
+}
+
---
+++ PathDeducer/MainForm.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PathDeducer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button_SelectExcel_Click(object sender, EventArgs e)
+        {
+            
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                openExcel(openFileDialog.FileName);
+            }
+
+            
+        }
+
+        private void openExcel(string path)
+        {
+            Microsoft.Office.Interop.Excel.Application oXL;
+            Microsoft.Office.Interop.Excel._Workbook oWB;
+            Microsoft.Office.Interop.Excel._Worksheet oSheet;
+
+            try
+            {
+                oXL = new Microsoft.Office.Interop.Excel.Application();
+                oWB = oXL.Workbooks.Add(path);
+                oSheet = oWB.ActiveSheet;
+                oXL.Visible = true;
+
+                var emptyIndex = getEmptyCellIndex(oSheet);
+                var emptyRowIndex = getEmptyRowIndex(oSheet);
+                var storyIndex = getPathCellIndex(oSheet);
+
+                if(storyIndex != -1 && emptyIndex != -1)
+                {
+                    oSheet.Cells[1, emptyIndex] = "StoryPathID";
+                    for (int i = 2; i < emptyRowIndex; i++)
+                    {
+                        var storyPath = (string)oSheet.Cells[i, storyIndex].Value;
+                        if (storyPath == null) continue;
+                        var pathID = getPathIdentifier(storyPath);
+                        oSheet.Cells[i, emptyIndex] = pathID;
+                    }
+                }
+
+                Debug.WriteLine($"Empty: {emptyIndex}");
+                Debug.WriteLine($"Story: {storyIndex}");
+                Debug.WriteLine($"Rows: {emptyRowIndex}");
+            }
+            catch (Exception theException)
+            {
+                String errorMessage;
+                errorMessage = "Error: ";
+                errorMessage = String.Concat(errorMessage, theException.Message);
+                errorMessage = String.Concat(errorMessage, " Line: ");
+                errorMessage = String.Concat(errorMessage, theException.Source);
+
+                MessageBox.Show(errorMessage, "Error");
+            }
+        }
+
+        private int getEmptyCellIndex(Microsoft.Office.Interop.Excel._Worksheet oSheet)
+        {
+            for (int index = 1; index < oSheet.Cells.Columns.Count; index++)
+            {
+                dynamic cellContent = oSheet.Cells[1, index].Value;
+                if (cellContent == null) return index;
+            }
+
+            return -1;
+        }
+
+        private int getEmptyRowIndex(Microsoft.Office.Interop.Excel._Worksheet oSheet)
+        {
+            for (int index = 1; index < oSheet.Cells.Rows.Count; index++)
+            {
+                dynamic cellContent = oSheet.Cells[index, 1].Value;
+                if (cellContent == null) return index;
+            }
+
+            return -1;
+        }
+
+        private int getPathCellIndex(Microsoft.Office.Interop.Excel._Worksheet oSheet)
+        {
+            for(int index = 1; index < oSheet.Cells.Columns.Count; index++)
+            {
+                var cellContent = (string)oSheet.Cells[1, index].Value;
+                if (cellContent.Contains("storyPath")) return index;
+            }
+
+            return -1;
+        }
+
+        private int getPathIdentifier(string path)
+        {
+            int pathID = 0;
+            if (path.Contains("1b-Konsequenzen")) pathID += 1;
+            if (path.Contains("2b-Konsequenzen")) pathID += 2;
+            if (path.Contains("3b-Konsequenzen")) pathID += 4;
+            if (path.Contains("4b-Konsequenzen")) pathID += 8;
+
+            return pathID;
+        }
+    }
+}
